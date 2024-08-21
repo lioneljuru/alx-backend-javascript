6221d83205559b67e48f3ce966ae60a8061f06b8
lioneljuru <136335805+lioneljuru@users.noreply.github.com>
2024-08-21 06:47:25 +0300

    Update

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,10 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
 process.stdin.on('readable', () => {
-	const chunk = process.stdin.read();
-	if (chunk) {
-	  process.stdout.write(`Your name is: ${chunk}`);
-	}
+  const chunk = process.stdin.read();
+  if (chunk) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
 });
 
 process.stdin.on('end', () => {
