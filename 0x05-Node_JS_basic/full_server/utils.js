9310868ef663817791562d0b1f532eab3de3c4e3
lioneljuru <136335805+lioneljuru@users.noreply.github.com>
2024-08-21 06:15:38 +0300

    Update

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -9,13 +9,13 @@ import fs from 'fs';
  * }>}
  */
 const readDatabase = (dataPath) => new Promise((resolve, reject) => {
-	if (!dataPath) {
-		reject(new Error('Cannot load the database'));
-	}
-	if (dataPath) {
-		fs.readFile(dataPath, (err, data) => {
-			if (err) {
-				reject(new Error('Cannot load the database'));
+  if (!dataPath) {
+    reject(new Error('Cannot load the database'));
+  }
+  if (dataPath) {
+    fs.readFile(dataPath, (err, data) => {
+      if (err) {
+              reject(new Error('Cannot load the database'));
 			}
 			if (data) {
 				const fileLines = data
